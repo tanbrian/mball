29d3a91437fb6fc667338cdcaf62e3acffe29254
Brian Tan <tanrui.brian@gmail.com>
2015-09-11 14:38:18 -0700

    Integrate Sass
    Renamed existing CSS files to SCSS, added global SCSS files in public/stylesheets, updated environment configs for module and library SCSS files

--- config/env/all.js
+++ config/env/all.js
@@ -1,45 +1,45 @@
 'use strict';
 
 module.exports = {
-	app: {
-		title: 'mball',
-		description: 'UCSD Circle K Masquerade Ball 2015 website',
-		keywords: 'UCSD, Circle K, Masquerade Ball'
-	},
-	port: process.env.PORT || 3000,
-	templateEngine: 'swig',
-	sessionSecret: 'MEAN',
-	sessionCollection: 'sessions',
-	assets: {
-		lib: {
-			css: [
-				'public/lib/bootstrap/dist/css/bootstrap.css',
-				'public/lib/bootstrap/dist/css/bootstrap-theme.css',
-			],
-			js: [
-				'public/lib/angular/angular.js',
-				'public/lib/angular-resource/angular-resource.js', 
-				'public/lib/angular-cookies/angular-cookies.js', 
-				'public/lib/angular-animate/angular-animate.js', 
-				'public/lib/angular-touch/angular-touch.js', 
-				'public/lib/angular-sanitize/angular-sanitize.js', 
-				'public/lib/angular-ui-router/release/angular-ui-router.js',
-				'public/lib/angular-ui-utils/ui-utils.js',
-				'public/lib/angular-bootstrap/ui-bootstrap-tpls.js'
-			]
-		},
-		css: [
-			'public/modules/**/css/*.css'
-		],
-		js: [
-			'public/config.js',
-			'public/application.js',
-			'public/modules/*/*.js',
-			'public/modules/*/*[!tests]*/*.js'
-		],
-		tests: [
-			'public/lib/angular-mocks/angular-mocks.js',
-			'public/modules/*/tests/*.js'
-		]
-	}
+  app: {
+    title: 'mball',
+    description: 'UCSD Circle K Masquerade Ball 2015 website',
+    keywords: 'UCSD, Circle K, Masquerade Ball'
+  },
+  port: process.env.PORT || 3000,
+  templateEngine: 'swig',
+  sessionSecret: 'MEAN',
+  sessionCollection: 'sessions',
+  assets: {
+    lib: {
+      scss: [
+        'public/lib/bootstrap-sass/assets/stylesheets/_bootstrap.scss',
+        'public/lib/bootstrap-sass/assets/stylesheets/_bootstrap-sprockets.scss'
+      ],
+      js: [
+        'public/lib/angular/angular.js',
+        'public/lib/angular-resource/angular-resource.js',
+        'public/lib/angular-cookies/angular-cookies.js',
+        'public/lib/angular-animate/angular-animate.js',
+        'public/lib/angular-touch/angular-touch.js',
+        'public/lib/angular-sanitize/angular-sanitize.js',
+        'public/lib/angular-ui-router/release/angular-ui-router.js',
+        'public/lib/angular-ui-utils/ui-utils.js',
+        'public/lib/angular-bootstrap/ui-bootstrap-tpls.js'
+      ]
+    },
+    css: [
+      'public/dist/application.css'
+    ],
+    js: [
+      'public/config.js',
+      'public/application.js',
+      'public/modules/*/*.js',
+      'public/modules/*/*[!tests]*/*.js'
+    ],
+    tests: [
+      'public/lib/angular-mocks/angular-mocks.js',
+      'public/modules/*/tests/*.js'
+    ]
+  }
 };
