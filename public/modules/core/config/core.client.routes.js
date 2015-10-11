29297a72cfb8d0e2e59cf46fade4a8e6d177c97f
Brian Tan <tanrui.brian@gmail.com>
2015-10-11 03:03:21 -0700

    Made mockup of FAQ section

--- public/modules/core/config/core.client.routes.js
+++ public/modules/core/config/core.client.routes.js
@@ -13,9 +13,9 @@ angular.module('core').config(['$stateProvider', '$urlRouterProvider',
       url: '/',
       templateUrl: 'modules/core/views/home.client.view.html'
     }).
-    state('schedule', {
-      url: '/schedule',
-      templateUrl: 'modules/core/views/schedule.client.view.html'
+    state('faq', {
+      url: '/faq',
+      templateUrl: 'modules/core/views/faq.client.view.html'
     });
   }
 ]);
