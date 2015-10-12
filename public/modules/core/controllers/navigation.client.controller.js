7bd52261b0e7a4f001e128aadbac6dad9bf8bd82
Brian Tan <tanrui.brian@gmail.com>
2015-10-11 23:00:28 -0700

    Added state change animation

---
+++ public/modules/core/controllers/navigation.client.controller.js
@@ -0,0 +1,26 @@
+(function() {
+    'use strict';
+
+    // TODO Convert to directive following Angular Style Guide
+    // Joseph if you're looking at this code forgive me pls
+
+    angular
+      .module('core')
+      .controller('NavigationController', ['$scope', '$location', '$rootScope', 'smoothScroll', function($scope, $location, $rootScope, smoothScroll) {
+        $scope.test = 'value';
+
+        $scope.scrollOrLoad = function(scrollDestId) {
+          $location.path('/#!');
+
+          angular.element(document).ready(function () {
+            var element = document.getElementById(scrollDestId);
+
+            var options = {
+              offset: 75
+            }
+
+            smoothScroll(element, options);
+          });
+        }
+      }]);
+})();
